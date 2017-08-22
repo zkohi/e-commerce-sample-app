4fc1d185cde5ae7f751359995b582e2eddb8ad41
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-08-23 00:54:39 +0900

    Add LineItem Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 require 'carrierwave/orm/activerecord'
 
 class Product < ApplicationRecord
+  has_many :line_items
+  has_many :orders, through: :line_items
+
   mount_uploader :filename, ProductUploader
 end
