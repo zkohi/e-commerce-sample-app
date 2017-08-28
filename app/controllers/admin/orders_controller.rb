36677ae32cded073d09aae8e03f1c2e08f087084
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-08-29 02:21:06 +0900

    Fix admin templates

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,9 +6,4 @@ class Admin::OrdersController < Admin::ApplicationController
   def show
     @order = Order.find(params[:id])
   end
-
-  private
-    def order_params
-      params.require(:order).permit(:user_id, :state, :shipment_state, :payment_state, :item_count, :item_total, :shipment_total, :payment_total, :tax_total, :total)
-    end
 end
