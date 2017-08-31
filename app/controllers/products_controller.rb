7d3fd9555eeb226f623691766d40c3e037ec55ca
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-08-31 22:49:55 +0900

    Add flg_non_display enum to product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,11 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @products = Product.order("sort_order DESC").where(flg_non_display: false).page(params[:page])
+    @products = Product.display.page(params[:page]).order("sort_order DESC")
   end
 
   def show
-    @product = Product.find(params[:id])
+    @product = Product.display.find(params[:id])
     @order = Order.new
     @order.line_items.build
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,10 +6,14 @@ class Product < ApplicationRecord
 
   mount_uploader :img_filename, ProductUploader
 
+  enum flg_non_display: {
+    display: false,
+    non_display: true
+  }
+
   validates :name, presence: true, length: { maximum: 30 }
   validates :img_filename, allow_blank: true, length: { maximum: 30 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than: 1000000 }
   validates :description, presence: true, length: { maximum: 500 }
-  validates :flg_non_display, presence: true, inclusion: { in: [true, false] }
   validates :sort_order, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 1000000 }
 end
