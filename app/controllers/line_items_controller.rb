d2806bd2c0166355431a2f71867d57f4b614164d
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-09-20 00:32:07 +0900

    orders及びline_itemsの処理をorders_controllerに集約

--- app/controllers/line_items_controller.rb
+++
@@ -1,8 +0,0 @@
-class LineItemsController < ApplicationController
-  before_action :authenticate_user!
-
-  def destroy
-    current_user.orders.cart.first.update_for_delete_line_item!(params[:id])
-    redirect_to cart_path, notice: '商品が削除されました'
-  end
-end
