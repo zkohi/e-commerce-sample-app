137c8d430a90162dfb59a6f95af23dfb30e7dc79
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-10-24 15:41:55 +0900

    ユーザーのニックネームとプロフィール画像の登録機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      if devise_controller? && resource_name != :admin
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :zipcode, :address])
+      if devise_controller? && resource_name != :company
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :zipcode, :address, :nickname, :img_filename])
       end
     end
 
