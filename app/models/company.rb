8323d20e630fca3dfdb2b2c939711f2780ec18c1
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-10-30 17:29:49 +0900

    モデルにdependentの設定を追加

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
   has_many :product_stocks, dependent: :destroy, inverse_of: :company
-  has_many :orders
+  has_many :orders, dependent: :nullify, inverse_of: :company
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
